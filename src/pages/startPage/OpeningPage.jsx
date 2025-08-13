e1f17c56ec02e3f447d51367f75aa0cea9b41e51
hongsujin2eeZyo <hong266770@gmail.com>
2025-08-13 16:54:40 +0900

    startpage

--- src/pages/startPage/OpeningPage.jsx
+++ src/pages/startPage/OpeningPage.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React,{useEffect} from 'react';
+import { useNavigate } from 'react-router-dom';
 import './OpeningPage.css';
 
 function OpeningPage() {
+    const navigate = useNavigate();
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            navigate('/landing');
+        }, 2000);
+
+        return () => clearTimeout(timer);
+    }, [navigate]);
     return (
         <div className='phone'>
             <img className='flagitLogo'src="/img/flagitLogo.svg" alt="logo" /> 
