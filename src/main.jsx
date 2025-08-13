0d969fb7107e946e16024ca20a08c9cfc6da5fee
ByeongYun0608 <byeongyun0608@gmail.com>
2025-08-13 15:46:45 +0900

    LandingPage 추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Opening from './pages/startPage/OpeningPage'
+import LoginPage from './pages/signupLogin/LoginPage'
+import DetailedLocationPage from './pages/homePage/DetailedLocationPage'
+import LevelListPage from './pages/myPage/LevelListPage'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Opening />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Opening />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/detailed-location" element={<DetailedLocationPage />} />
+        <Route path="/level-list" element={<LevelListPage />} />
+      </Routes>
+    </Router>
   </StrictMode>,
 )
---
+++ src/pages/startPage/LandingPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function LandingPage() {
+  return (
+    <div>시작페이지 입니다</div>
+  );
+}
+
+export default LandingPage;
