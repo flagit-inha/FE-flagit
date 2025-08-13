e9da7cd1ad818992604fe3fb0705dc8cc8ed7cad
hongsujin2eeZyo <hong266770@gmail.com>
2025-08-13 14:00:38 +0900

    기반다지기 큰단락폴더만 생성함

---
+++ src/startPage/Opening.jsx
@@ -0,0 +1,12 @@
+function Opening() {
+    return (
+        <div className="opening">
+        <h1>Welcome to Our Application</h1>
+        <p>This is the starting point of your journey.</p>
+        <button onClick={() => alert('Let\'s get started!')}>Get Started</button>
+        </div>
+    );
+
+}
+
+export default Opening;
