da9fe6411cfea1a7cd8c1d9aa8f35225b3cc82d6
hongsujin2eeZyo <hong266770@gmail.com>
2025-08-13 14:10:45 +0900

    폴더생성함

--- src/startPage/Opening.jsx
+++ src/startPage/Opening.jsx
@@ -1,9 +1,10 @@
+import React from 'react';
+import './Opening.css';
+
 function Opening() {
     return (
-        <div className="opening">
-        <h1>Welcome to Our Application</h1>
-        <p>This is the starting point of your journey.</p>
-        <button onClick={() => alert('Let\'s get started!')}>Get Started</button>
+        <div className="background">
+            <div className='phone'></div>
         </div>
     );
 
