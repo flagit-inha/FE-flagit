21ec5cfadaf8d720cd5907520fef4177e1fac4aa
hongsujin2eeZyo <hong266770@gmail.com>
2025-08-13 14:46:21 +0900

    .

---
+++ src/pages/startPage/Opening.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Opening.css';
+
+function Opening() {
+    return (
+        <div className="background">
+            <div className='phone'></div>
+        </div>
+    );
+}
+
+export default Opening;
