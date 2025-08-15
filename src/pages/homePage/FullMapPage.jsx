26ac3f5a1553728361f05dea3d6bb985827a503f
ByeongYun0608 <byeongyun0608@gmail.com>
2025-08-15 22:16:36 +0900

    fullmap 90프로 완료

--- src/pages/homePage/FullMapPage.jsx
+++ src/pages/homePage/FullMapPage.jsx
@@ -1,63 +1,66 @@
 import React from 'react';
 import './FullMapPage.css';
 
-function FullMapPage() {
+function FullMapPage({ userName = '홍길동' }) {
+  const line1 = `${userName}님의`;
+  const line2 = '기록';
+  const ariaTitle = `${line1} ${line2}`;
   return (
     <div className="fullmap-container">
-      <div className="fullmap-content">
-        <div className="fullmap-header">
-          <h1>홍길동님의 기록</h1>
-        </div>
+      <div className="top-bar">
+        <img src="/img/flagit2.svg" alt="Flagit 로고" className="app-logo" draggable="false" />
+        <h1 className="top-title" aria-label={ariaTitle}>
+          <span className="title-line1">{line1}</span>
+          <span className="title-line2">{line2}</span>
+        </h1>
+      </div>
 
+      <div className="fullmap-content">
         <div className="map-area">
           <div className="geo-wrapper">
             <div className="geo-canvas">
-              {/* 메인 지도 (object 대신 img로 안정화) */}
-              <img
-                src="/img/main_map.svg"
-                alt="대한민국 지도"
-                className="geo-main"
-                draggable="false"
-              />
-
-              {/* 독도 / 제주 */}
+              <img src="/img/main_map.svg" alt="대한민국 지도" className="geo-main" draggable="false" />
               <img src="/img/dokdo.svg" alt="독도" className="geo-dokdo" draggable="false" />
               <img src="/img/jeju.svg" alt="제주도" className="geo-jeju" draggable="false" />
-
-              {/* 구름 */}
               <img src="/img/cloud.svg" alt="" className="cloud cloud-top" />
               <img src="/img/cloud.svg" alt="" className="cloud cloud-middle" />
               <img src="/img/cloud.svg" alt="" className="cloud cloud-bottom" />
             </div>
           </div>
-
-          <div className="map-footer">
-            <div className="footer-text">내 누적 기록: 13개</div>
-            <button className="add-button" aria-label="기록 추가">+</button>
-          </div>
         </div>
       </div>
 
+      <div className="map-footer">
+        <div className="footer-flag">
+          <img src="/img/running2.svg" alt="" className="footer-flag-icon" draggable="false" />
+          <div className="footer-flag-text">내 누적 깃발</div>
+        </div>
+        {/* 기존 '+' 텍스트 제거: 이미지 아이콘만 */}
+        <button className="add-button" aria-label="기록 추가">
+          <img src="/img/plus.svg" alt="" aria-hidden="true" className="add-button-icon" draggable="false" />
+        </button>
+      </div>
+
       <div className="bottom-nav">
-        <div className="nav-item">
-          <img src="/img/route-icon.svg" alt="경로" />
-          <span>경로</span>
+        {/* 로고 아직 없으니 center-logo-slot 제거 */}
+        <div className="nav-item active">
+          <img src="/img/route.svg" alt="route" />
+          <span>route</span>
         </div>
         <div className="nav-item">
-          <img src="/img/home-icon.svg" alt="홈" />
-          <span>홈</span>
+          <img src="/img/home.svg" alt="home" />
+          <span>home</span>
         </div>
         <div className="nav-item">
-          <img src="/img/mycrew-icon.svg" alt="내 크루" />
-          <span>내 크루</span>
+          <img src="/img/mycrew.svg" alt="mycrew" />
+          <span>mycrew</span>
         </div>
         <div className="nav-item">
-          <img src="/img/my-icon.svg" alt="내 정보" />
-          <span>내 정보</span>
+          <img src="/img/my.svg" alt="my" />
+          <span>my</span>
         </div>
       </div>
     </div>
   );
 }
-
 export default FullMapPage;
