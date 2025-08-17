9a487d4cf6fac172f348379d4328d519f07f66a4
ByeongYun0608 <byeongyun0608@gmail.com>
2025-08-17 21:41:33 +0900

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import AppRoutes from './route/AppRoutes'  // 경로 수정
 
+{/*main.jsx 코드*/}
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
       <AppRoutes />
     </Router>
   </StrictMode>,
-)
+) 
