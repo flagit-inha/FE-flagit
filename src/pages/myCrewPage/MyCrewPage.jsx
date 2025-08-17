69dfe7042e78458b41c73870821f910f9c2b5997
hongsujin2eeZyo <hong266770@gmail.com>
2025-08-17 23:55:36 +0900

    조금 더 함 . 하기시르당킼

--- src/pages/myCrewPage/MyCrewPage.jsx
+++ src/pages/myCrewPage/MyCrewPage.jsx
@@ -3,22 +3,38 @@ import './MyCrewPage.css';
 function MyCrewPage() {
   return (
     <div>
+      {/* 크루이미지 */}
       <img src="/img/crep.png" alt="mycrew" className="mycrew-icon" />
+
+
+      {/* 크루프로필  */}
       <div className='crewProfile'>
         <img src="/img/profile.png" alt="profile" className="profile-image" /> 
-        
       </div>
-  
 
+      {/* 크루이름 */}
+      <div className='crewName'>
+        <span className='crewNameText'>빌려온 깃냥이 </span>
+        <img src='/img/fxemoji_fire.svg' alt='crew' className='fireIcon' />
+      </div>
 
+      {/* 크루 총원 */}
+      <div className='crewMemberCount'>
+        <span className='memberCountText'>총원: 50명</span>
+      </div>
 
+      <div className='notic-member'>
+        <button className='noticeButton'>공지</button>
+        <button className='memberButton'>회원</button>
+        <div></div>
+        <button className='list'>공지사항</button>
+      </div>
 
+     
+   
+      
 
-
-
-
-
-
+      {/* 하단네비게이션 바 */}
       <div className="bottom-nav">
         <div className="nav-item active">
           <img src="/img/route.svg" alt="route" />
