e9ce27e3a2532a0add765c81ff3d64fdd3197c04
hongsujin2eeZyo <hong266770@gmail.com>
2025-08-17 22:14:58 +0900

    브랜치생성테스트'

--- src/pages/myCrewPage/MyCrewPage.jsx
+++ src/pages/myCrewPage/MyCrewPage.jsx
@@ -2,7 +2,27 @@ import React from 'react';
 
 function MyCrewPage() {
   return (
-    <div>마이크루 페이지</div>
+    <div>마이크루 페이지
+
+      <div className="bottom-nav">
+        <div className="nav-item active">
+          <img src="/img/route.svg" alt="route" />
+          <span>route</span>
+        </div>
+        <div className="nav-item">
+          <img src="/img/home.svg" alt="home" />
+          <span>home</span>
+        </div>
+        <div className="nav-item">
+          <img src="/img/mycrew.svg" alt="mycrew" />
+          <span>mycrew</span>
+        </div>
+        <div className="nav-item">
+          <img src="/img/my.svg" alt="my" />
+          <span>my</span>
+        </div>
+      </div>
+    </div>
   );
 }
 
