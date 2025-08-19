3d25566d2b6cd141cdb3926788cf6e8c765058c1
yewonee <hyewon8685@naver.com>
2025-08-19 22:26:14 +0900

    style: 돋보기페이지디자인 완성

--- src/pages/routePage/SearchingCrewsPage.jsx
+++ src/pages/routePage/SearchingCrewsPage.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import { useNavigate } from "react-router-dom";
+import "./SearchingCrewsPage.css";
 
-function SearchingCrewPage() {
+export default function SearchingCrewsPage() {
+  const nav = useNavigate();
+
   return (
-    <div>
-      <h1>주변 크루 탐색 중...</h1>
+    <div className="search-screen">
+      <div className="search-card">
+        <button
+          className="search-back"
+          aria-label="뒤로가기"
+          onClick={() => nav(-1)}
+        >
+          ‹
+        </button>
+
+        {/* 돋보기 아이콘 (이미지 파일 경로만 맞춰줘) */}
+        <img
+          src="/img/question.svg"
+          alt="검색 돋보기"
+          className="search-icon"
+        />
+
+        <p className="search-text">주변 크루 탐색 중...</p>
+      </div>
     </div>
   );
 }
-
-export default SearchingCrewPage;
