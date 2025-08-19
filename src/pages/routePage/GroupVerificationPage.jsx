47a5c4df856f87f3261865381dec41fadb8e4107
yewonee <hyewon8685@naver.com>
2025-08-19 22:20:56 +0900

    syle: 단체인증 페이지 디자인 완료

--- src/pages/routePage/GroupVerificationPage.jsx
+++ src/pages/routePage/GroupVerificationPage.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import { useNavigate } from "react-router-dom";
+import "./GroupVerificationPage.css";
 
-function GroupVerificationPage() {
+export default function GroupVerificationPage() {
+  const nav = useNavigate();
+
   return (
-    <div>
-      <h1>단체 인증 페이지</h1>
+    <div className="gver-screen">
+      <div className="gver-card">
+        {/* 뒤로가기 */}
+        <button
+          className="gver-back"
+          aria-label="뒤로가기"
+          onClick={() => nav(-1)}
+        >
+          ‹
+        </button>
+
+        {/* 헤더 */}
+        <h1 className="gver-title">단체 인증</h1>
+        <p className="gver-desc">
+          가게 근처에서 단체 인증을 완료하고
+          <br />
+          할인쿠폰을 발급받으세요
+        </p>
+
+        {/* 일러스트 (이미지는 public/img 안에 두고 경로만 맞춰줘) */}
+        <img
+          className="gver-illu"
+          src="/img/map_person.svg"
+          alt="단체 인증 일러스트"
+        />
+
+        {/* CTA */}
+        <button className="gver-cta" onClick={() => nav("/qr")}>
+          인증하기
+        </button>
+      </div>
     </div>
   );
 }
-
-export default GroupVerificationPage;
