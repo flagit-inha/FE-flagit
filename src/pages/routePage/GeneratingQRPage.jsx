ce3d2dd591db3b6311b1fbd9b384f57cf6413a76
yewonee <hyewon8685@naver.com>
2025-08-20 14:18:05 +0900

    style: 큐알 로딩페이지 디자인완료

--- src/pages/routePage/GeneratingQRPage.jsx
+++ src/pages/routePage/GeneratingQRPage.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import "./GeneratingQRPage.css";
 
-function GeneratingQRPage() {
+
+export default function GeneratingQRPage() {
   return (
-    <div>
-      <h1>QR 발급 중...</h1>
+    <div className="qr-screen">
+      <div className="qr-content">
+        <img src={"/img/check.svg"} alt="체크" className="qr-icon" />
+        <p className="qr-text">단체 인증 완료! QR 발급 중...</p>
+      </div>
     </div>
   );
 }
-
-export default GeneratingQRPage;
